4f18ee723119546b2737ff5fa28e963afd4dd5b2
Fabian Faßbender <fabian.fassbender42@googlemail.com>
2018-12-30 13:22:45 +0100

    Update to JDK 11

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module jraknet {
+    requires slf4j.api;
+
+    requires io.netty.transport;
+    requires io.netty.buffer;
+    requires io.netty.transport.epoll;
+    requires io.netty.common;
+
+    exports io.gomint.jraknet;
+}
